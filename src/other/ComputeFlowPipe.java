a811444c904b9a735485557e4899d8c8568c4645
pedroth <pedrotiago92@gmail.com>
2017-09-16 11:02:27 +0100

    refactor compute flow

--- src/other/ComputeFlowPipe.java
+++ src/other/ComputeFlowPipe.java
@@ -1,12 +1,11 @@
 package other;
 
-import algorithms.QuickSortWithPermutation;
 import graph.Graph;
+import graph.TopologicalOrder;
 import inputOutput.TextIO;
 import javafx.util.Pair;
 
 import java.io.IOException;
-import java.util.Stack;
 
 public class ComputeFlowPipe {
     public static void main(String[] args) throws IOException {
@@ -25,7 +24,7 @@ public class ComputeFlowPipe {
             graph.addEdge(u, v);
             graph.putEdgeProperty(new Pair<>(u, v), "flow", Integer.parseInt(params[2]));
         }
-        Integer[] topologicalOrder = topologicalOrder(graph);
+        Integer[] topologicalOrder = TopologicalOrder.topologicalOrder(graph);
         for (Integer u : graph.getVertexSet()) {
             graph.putVertexProperty(u, "flowAcc", 0);
             for (Integer v : graph.getAdjVertex(u)) {
@@ -40,59 +39,5 @@ public class ComputeFlowPipe {
                 System.out.printf("accumulatedFlow(%d, %d) =  %d \n", u, v, update);
             }
         }
-    }
-
-    private static Integer[] topologicalOrder(Graph graph) {
-        int time = 0;
-        Stack<Integer> stack = new Stack<>();
-        Stack<Integer> auxStack = new Stack<>();
-        for (Integer u : graph.getVertexSet()) {
-            graph.putVertexProperty(u, "start", Integer.MIN_VALUE);
-            graph.putVertexProperty(u, "end", Integer.MIN_VALUE);
-        }
-        for (Integer u : graph.getVertexSet()) {
-            if ((int) graph.getVertexProperty(u, "start") >= 0) {
-                continue;
-            }
-            stack.add(u);
-            // DFS
-            while (!stack.isEmpty()) {
-                int w = stack.pop();
-                if ((int) graph.getVertexProperty(w, "start") < 0) {
-                    graph.putVertexProperty(w, "start", time++);
-                    for (Integer v : graph.getAdjVertex(w)) {
-                        if ((int) graph.getVertexProperty(v, "start") > 0 && (int) graph.getVertexProperty(v, "end") < 0) {
-                            throw new RuntimeException("The graph has cycles ... please put a DAG( direct acyclic graph ).");
-                        }
-                        //v.end < 0 whenever v.start < 0 by construction
-                        if ((int) graph.getVertexProperty(v, "start") < 0) {
-                            auxStack.push(v);
-                        }
-                    }
-                }
-                if (isFinished(w, graph)) {
-                    graph.putVertexProperty(w, "end", ++time);
-                } else {
-                    stack.push(w);
-                    while (!auxStack.empty()) {
-                        stack.push(auxStack.pop());
-                    }
-                }
-            }
-        }
-        QuickSortWithPermutation quickSortWithPermutation = new QuickSortWithPermutation(QuickSortWithPermutation.DECREASING_ORDER);
-        Integer[] ends = graph.getVertexPropertyMap("end").toArray(new Integer[graph.getNumVertex()]);
-        quickSortWithPermutation.sort(ends);
-        Integer[] topologicalOrder = graph.getKeyIndex();
-        return quickSortWithPermutation.permutate(topologicalOrder);
-    }
-
-    private static boolean isFinished(int u, Graph graph) {
-        for (Integer v : graph.getAdjVertex(u)) {
-            if ((int) graph.getVertexProperty(v, "start") < 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
